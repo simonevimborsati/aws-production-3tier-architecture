c4754b0fa78c3ff27ac050471b752dd150fb4114
simonevimborsati <simonevimborsati@gmail.com>
2026-08-30 12:58:35 +0200

    security: pass DB password via sensitive variable

--- database.tf
+++ database.tf
@@ -14,8 +14,8 @@ resource "aws_db_instance" "postgres_db" {
   engine_version         = "15"
   instance_class         = "db.t3.micro"       
   db_name                = "appdb"
-  username               = "var.db_username"
-  password               = "var.db_password" 
+  username               = var.db_username
+  password               = var.db_password 
   db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.name
   vpc_security_group_ids = [aws_security_group.db_sg.id]
   skip_final_snapshot    = true    
