300fd6a5f480ea59f893cad19b7a62a311ef8e8d
simonevimborsati <simonevimborsati@gmail.com>
2026-08-30 12:50:14 +0200

    fix: deploy EC2 instances across all 3 private subnets

--- compute.tf
+++ compute.tf
@@ -15,21 +15,23 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"]
 }
 
-# 2. Creazione dell'istanza EC2 nella prima Subnet Privata
+# 2. Creazione delle istanze EC2 nelle Subnet Private (1 per ogni AZ)
 resource "aws_instance" "app_server" {
+  count                  = length(module.vpc.private_subnets)
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
-  subnet_id              = module.vpc.private_subnets[0]
+  subnet_id              = module.vpc.private_subnets[count.index]
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
   tags = {
-    Name = "web-app-backend"
+    Name = "web-app-backend-${count.index + 1}"
   }
 }
 
-# 3. Collegamento (Attachment) dell'istanza EC2 al Target Group dell'ALB
+# 3. Collegamento (Attachment) di tutte le istanze EC2 al Target Group dell'ALB
 resource "aws_lb_target_group_attachment" "app_attachment" {
+  count            = length(aws_instance.app_server)
   target_group_arn = aws_lb_target_group.my_target_group.arn
-  target_id        = aws_instance.app_server.id
+  target_id        = aws_instance.app_server[count.index].id
   port             = 80
 }
